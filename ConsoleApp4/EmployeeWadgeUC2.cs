ff6851cb60f717e0b0375b3825ecf7faf18d1d3b
Divya <beingdivyadixit@gmail.com>
2022-12-15 21:50:34 +0530

    Added Calculate Employee Wadge Problem By Using Random (UC2)

---
+++ ConsoleApp4/EmployeeWadgeUC2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class EmployeeWadgeUC2
+    {
+         public static void EmployeeUC2(int empCheck)
+        {
+            int IS_FULL_TIME = 1, EMP_RATE_PER_HOUR = 20;
+            int empWadge = 0, empHr = 0;
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHr=8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+            empWadge = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wadge:" +empWadge);
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,8 +6,10 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for UC1");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for Employee Attendance UC1 \n Enter 2 for Calculating Employee Wage UC2");
             int option = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
             switch (option)
             {
 
@@ -15,9 +17,10 @@ namespace ConsoleApp4
                       Console.WriteLine("Welcome to Employee Wage Computation Program");
                     break;
                 case 1:
-                    Random random= new Random();
-                    int empCheck=random.Next(0, 2);
                     EmployeeWadgeUC1.EmployeeUC1(empCheck);
+                    break;
+                case 2:
+                    EmployeeWadgeUC2.EmployeeUC2(empCheck);
                     break;
                 default:
                     Console.WriteLine("Please Input Correct option");
