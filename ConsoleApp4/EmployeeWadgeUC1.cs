5854a4c807f5c495cea2c85890b16a288ed46e35
Divya <beingdivyadixit@gmail.com>
2022-12-15 21:25:06 +0530

    UC1 program (Check Employee Attnd. by using random)

---
+++ ConsoleApp4/EmployeeWadgeUC1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class EmployeeWadgeUC1
+    {
+        public static void EmployeeUC1(int empCheck)
+        {
+            int IS_FULL_TIME = 1;
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Present");
+            }
+        }
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine(" Enter 0 for welcome program \n Enter 1 for UC1");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+
+                case 0:
+                      Console.WriteLine("Welcome to Employee Wage Computation Program");
+                    break;
+                case 1:
+                    Random random= new Random();
+                    int empCheck=random.Next(0, 2);
+                    EmployeeWadgeUC1.EmployeeUC1(empCheck);
+                    break;
+                default:
+                    Console.WriteLine("Please Input Correct option");
+                    break;
+
+            }
         }
+
     }
 }
