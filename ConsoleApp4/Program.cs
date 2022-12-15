c721f039574386996178bc602f094a3eba23e89b
Divya <beingdivyadixit@gmail.com>
2022-12-15 18:36:01 +0530

    Added Welcome program

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
